d137466a514aa4e957486d0be0f2d32a3b277787
aoheng yang <yykk772@gmail.com>
2021-10-04 16:53:25 -0400

    python version

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+noun = input("Gimme a noun...")
+adjective = input("Okay how about an adjective")
+adverb = input("cai i get an adverb too?")
+verb = input("And finally, what about verb")
+            
+print("On a troppical " + str(noun) + ",")
+print("Underneath a " + str(adjective) + "lava moon.")
+print("hangin' " + str(adverb) + "with the hulu dancers")
+print(str(verb) + " question cause' thry all get the answers.")
